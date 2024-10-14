382d96993c2dec7f45be47b1abf7d8279a57bad3
Himanshu-Lilhore <himanshulilhore@gmail.com>
2024-10-14 22:44:46 +0530

    working app

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
+require('dotenv').config()
 const express = require('express')
+const app = express()
 const http = require('http')
 const socketIo = require('socket.io')
 const cors = require('cors')
-
-const app = express()
+const port = process.env.PORT || 3000
+const { v4: uuidv4 } = require('uuid');
+users = {}
 
 app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST'],
-  credentials: true
+	origin: 'http://localhost:5173',
+	methods: ['GET', 'POST'],
+	credentials: true
 }))
 
 app.use(express.json())
@@ -16,31 +19,67 @@ app.use(express.json())
 const messages = []
 
 app.get('/api/messages', (req, res) => {
-  res.json(messages)
+	res.json(messages)
 })
 
 const server = http.createServer(app)
 const io = socketIo(server, {
-  cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
-    credentials: true
-  }
+	connectionStateRecovery: {},
+	cors: {
+		origin: 'http://localhost:5173',
+		methods: ['GET', 'POST'],
+		credentials: true
+	}
 })
 
+
 io.on('connection', (socket) => {
-  console.log(`${socket.id} user connected`)
+	console.log(`${socket.id} user connected`)
 
-  socket.on('message', (message) => {
-    messages.push({id: socket.id, message: message})
-    io.emit('message', {id: socket.id, message: message})
-  })
+	socket.on('checkUsernameAvailability', (userID, callback) => {
+		console.log(users)
+		const usernamesTaken = Object.keys(users).map(key => users[key])
+		let isTaken = true
+		isTaken = usernamesTaken.find(username => username===userID)
+		if(!isTaken) {
+			messages.map(msg => {
+				if(msg.id === users[socket.id]) msg.id = userID
+			})
+			users[socket.id] = userID
+			io.emit('refresh')
+			console.log(`new username set : ${userID}`)
+		} else {
+			console.log('username was already taken')
+		}
+		callback(null, {
+			isTaken: isTaken
+		});
+	})
 
-  socket.on('disconnect', () => {
-    console.log(`${socket.id} user DISconnected`)
-  })
+    socket.on('request_user_id', () => {
+        const userId = uuidv4(); // Generate unique userId
+        socket.emit('user_id', userId);
+    });
+
+    socket.on('user_reconnect', (userId) => {
+        // Reassociate socket with the userId
+        users[socket.id] = userId;
+        console.log(`User ${userId} connected with socket ID ${socket.id}`);
+    });
+
+	socket.on('message', (message, callback) => {
+		messages.push({ id: users[socket.id], message: message })
+		io.emit('message', { id: users[socket.id], message: message })
+		callback({
+			status: 'ok'
+		});
+	})
+
+	socket.on('disconnect', () => {
+		console.log(`${socket.id} user DISconnected`)
+	})
 })
 
 server.listen(3000, () => {
-  console.log('listening on *:3000')
+	console.log(`Backend live : http://localhost:${port}/`)
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,66 +1,205 @@
 // src/App.js
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import socket from './socket';
 import Axios from 'axios';
 
 Axios.defaults.withCredentials = true;
 
 function App() {
-  const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState('');
+	const [messages, setMessages] = useState([]);
+	const [input, setInput] = useState('');
+	const [user, setUser] = useState(localStorage.getItem('userId') || '')
+	const [oldUsername, setOldUsername] = useState(user)
+	const [isConnected, setIsConnected] = useState(true)
+	let myTimer = useRef(null);
+	const [uniqueUsers, setUniqueUsers] = useState([]);
 
-  useEffect(() => {
-    
-    const fetchMessages = async () => {
-      try {
-        const response = await Axios.get('http://localhost:3000/api/messages');
-        console.log(response.data)
-        setMessages(response.data);
-      } catch (error) {
-        console.error('Error fetching messages:', error);
-      }
-    };
+	useEffect(() => {
+		let userId = localStorage.getItem('userId');
 
-    fetchMessages();
+		if (!userId) {
+			// Request a new userId from the server
+			socket.emit('request_user_id');
+			socket.on('user_id', (newUserId) => {
+				userId = newUserId;
+				localStorage.setItem('userId', newUserId);
+			});
+		} else {
+			// Reconnect with the existing userId
+			socket.emit('user_reconnect', userId);
+		}
+		setUser(userId)
+		setOldUsername(userId)
 
-    socket.on('message', (message) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
-    });
+		const fetchMessages = async () => {
+			try {
+				const response = await Axios.get('http://localhost:3000/api/messages');
+				console.log(response.data)
+				setMessages(response.data);
+			} catch (error) {
+				console.error('Error fetching messages:', error);
+			}
+		};
 
-    return () => {
-      socket.off('message');
-    };
-  }, []);
-
-  
-  const sendMessage = () => {
-    socket.emit('message', input);
-    console.log(`Sent message : ${input}`)
-    setInput('');
-  };
-
-
-  useEffect(() => {
-    console.log(`Messsage array got updated (frontend) : \n${messages}`)
-  }, [messages])
+		fetchMessages();
 
 
-  return (
-    <div>
-      <h1>Chat App</h1>
-      <div>
-        {messages.map((msg, index) => (
-          <div key={index}>{msg.id} : {msg.message}</div>
-        ))}
-      </div>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={sendMessage}>Send</button>
-    </div>
-  );
+		socket.on('refresh', () => {
+			window.location.reload();
+		});
+
+		socket.on('message', (message) => {
+			setMessages((prevMessages) => [...prevMessages, message]);
+		});
+
+
+		const handleConnect = () => {
+			setIsConnected(true);
+			console.log('Socket connected');
+		};
+
+		const handleDisconnect = () => {
+			setIsConnected(false);
+			console.log('Socket disconnected');
+		};
+
+		// Listen for socket connection/disconnection events
+		socket.on('connect', handleConnect);
+		socket.on('disconnect', handleDisconnect);
+
+		// Cleanup event listeners on component unmount
+		return () => {
+			socket.off('connect', handleConnect);
+			socket.off('disconnect', handleDisconnect);
+		};
+
+	}, []);
+
+
+	const sendMessage = () => {
+		socket.timeout(5000).emit('message', input, (err, response) => {
+			if (err) {
+				console.log('some issue is there');
+
+			} else {
+				console.log(response.status); // 'ok'
+			}
+		})
+		console.log(`Sent message : ${input}`)
+		setInput('');
+	};
+
+
+	const toggleConnect = (e) => {
+		e.preventDefault();
+		if (socket.connected) {
+			socket.disconnect();
+			console.log('disconnected')
+		} else {
+			socket.connect();
+			console.log('connected')
+		}
+	};
+
+
+	useEffect(() => {
+		console.log(`Messsage array got updated (frontend) : \n${messages}`)
+		const uniqueIds = [...new Set(messages.map(msg => msg.id))];
+		setUniqueUsers(uniqueIds);
+	}, [messages])
+
+	useEffect(() => {
+		console.log(`socket.connected : ${socket.connected}`)
+		if (socket.connected) setIsConnected(true)
+		else setIsConnected(false)
+	}, [socket])
+
+
+	function settingUsername(newUsername) {
+		const validValue = newUsername.replace(/[^a-zA-Z0-9]/g, '');
+		newUsername = validValue
+		setUser(validValue);
+		clearTimeout(myTimer.current);
+		console.log('timer cleared')
+
+		if (oldUsername !== '' && newUsername !== '' && oldUsername !== newUsername) {
+			console.log('setting timeout')
+			myTimer.current = setTimeout(() => {
+				console.log('timeout ran')
+				try {
+					socket.emit('checkUsernameAvailability', newUsername, (err, response) => {
+						if (err) {
+							console.log(err);
+
+						} else {
+							if (!(response.isTaken)) {
+								localStorage.setItem('userId', newUsername);
+								setOldUsername(newUsername)
+								console.log('New username set')
+							} else {
+								setUser(oldUsername)
+								console.log('Username was already taken')
+							}
+						}
+					})
+				} catch (err) {
+					console.log('Error checking username availability.')
+				}
+			}, 2000)
+		}
+	}
+
+
+
+	return (
+		<div className='flex flex-col gap-4 p-4 w-fit'>
+			<div className='text-4xl font-bold mb-4'>Chat App</div>
+			<div className='flex flex-col gap-3 border rounded-sm p-2'>
+				{/* chatroom info */}
+				<div className='flex flex-row gap-2 pt-1'>
+					<input className='border rounded-md px-2 bg-red-500/10 text-white max-w-40 h-fit' onChange={(e) => settingUsername(e.target.value)} value={user} />
+					<div className='flex flex-row gap-1 px-1 text-nowrap flex-wrap'>
+						<div>Talking to : </div>
+						{
+							uniqueUsers.map(val => {
+								if (user !== val)
+									return <div className='border px-2 h-fit rounded-full' key={val}>{val}</div>
+							})
+						}
+					</div>
+				</div>
+
+				<div className='border w-full'></div>
+
+				{/* messages  */}
+				<div className='flex flex-col gap-3'>
+					{messages.map((msg, index) => (
+						<div className={`flex w-full ${msg.id === oldUsername ? 'justify-end' : 'justify-start'}`} key={index}>
+							<div className='flex flex-col'>
+								{
+									msg.id !== oldUsername ?
+										<div className='text-xs px-1'>{msg.id}</div> : <></>
+								}
+								<div className={`${msg.id === oldUsername ? 'bg-green-500/50' : 'bg-blue-500/70'} max-w-48 rounded-md px-2 break-words`}>{msg.message}</div>
+							</div>
+						</div>
+					))}
+				</div>
+				{/* Inputs  */}
+				<div className='flex flex-row gap-2'>
+					<input
+						type="text"
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
+						className='rounded-md px-1 text-black'
+					/>
+					<button onClick={sendMessage} className='px-2 border rounded-md hover:bg-zinc-600'>Send</button>
+					<button onClick={(e) => toggleConnect(e)} className='px-2 border rounded-md hover:bg-zinc-600'>{isConnected ? 'X' : '>'}</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
